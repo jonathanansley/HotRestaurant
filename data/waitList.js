629252c45c526aca484953018114a0ef381d2846
Saveliy <samoylov3@hotmail.com>
2017-08-05 13:27:57 -0400

    removed dummy data

--- data/waitList.js
+++ data/waitList.js
@@ -1,16 +1,3 @@
-let waitList =[
-  {
-  "customerName": "Dude Duderson",
-  "phoneNumber": "78945644564",
-  "customerEmail": "dudesickle@gmail.com",
-  "customerID": "123456"
-  },
-  {
-  "customerName": "Crab Craberson",
-  "phoneNumber": "888-888-8888",
-  "customerEmail": "crabbyPatty@me.com",
-  "customerID": "987456"
-  }
-]
+let waitList =[]
 
 module.exports = waitList;
