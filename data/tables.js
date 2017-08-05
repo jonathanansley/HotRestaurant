f7a3ae97c5aec491a8a18a25da31f2bbd181e5a8
Saveliy <samoylov3@hotmail.com>
2017-08-05 10:49:45 -0400

    furthering file structure

---
+++ data/tables.js
@@ -0,0 +1,14 @@
+[
+{
+"customerName": "full stack",
+"phoneNumber": "78945644564",
+"customerEmail": "jk@gmail.com",
+"customerID": "test 2"
+},
+{
+"customerName": "Sharyn",
+"phoneNumber": "888-888-8888",
+"customerEmail": "sharyn@me.com",
+"customerID": "134547756"
+}
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const path = require('path')
 
 
 let app = express();
@@ -11,11 +12,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.get("/", (req, res)=>{
+app.use(express.static('public'))
 
+// Front Page
+app.get("/", (req, res)=>{
+  res.sendFile(path.join(__dirname), "index.html")
 })
 
+// Tables
+app.get("/tables", (req, res)=>{
+  res.sendFile(path.join(__dirname), "tables.html")
+})
 
+// Reservation
+app.get("/reserve", (req, res)=>{
+  res.sendFile(path.join(__dirname), "reservation.html")
+})
 
 
 app.listen(PORT, ()=>{
